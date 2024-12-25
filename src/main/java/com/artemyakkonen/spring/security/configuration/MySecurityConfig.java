59e61e3c3a04c52b8173fc80827cae9a623a661b
artem <iakonien@gmail.com>
2024-12-25 14:58:31 +0300

    Конфигурация приложения

---
+++ src/main/java/com/artemyakkonen/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,40 @@
+package com.artemyakkonen.spring.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class MySecurityConfig{
+
+@Bean
+    public UserDetailsService userDetailsService()  {
+   UserBuilder users = User.withDefaultPasswordEncoder();
+
+        UserDetails user1 = users
+                .username("zaur")
+                .password("zaur")
+                .roles("EMPLOYEE")
+                .build();
+        UserDetails user2 = users
+                .username("elena")
+                .password("elena")
+                .roles("HR")
+                .build();
+        UserDetails user3 = users
+                .username("ivan")
+                .password("ivan")
+                .roles("MANAGER", "HR")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
+    }
+}
