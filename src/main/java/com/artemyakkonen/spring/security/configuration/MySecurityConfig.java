ddd86ca1f071eb5d98d060bc6e467abb08fb91ab
artem <iakonien@gmail.com>
2024-12-27 14:55:20 +0300

    Авторизация

--- src/main/java/com/artemyakkonen/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/artemyakkonen/spring/security/configuration/MySecurityConfig.java
@@ -4,22 +4,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 @Configuration
 public class MySecurityConfig{
 
-@Bean
-public UserDetailsService userDetailsService()  {
-   UserBuilder users = User.withDefaultPasswordEncoder();
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((user) -> user
+                .requestMatchers("/").hasAnyRole("MANAGER", "HR", "EMPLOYEE")
+                .requestMatchers("/manager_info/**").hasRole("MANAGER")
+                .requestMatchers("/hr_info/**").hasRole("HR")
+                .anyRequest().authenticated()
+        ).formLogin(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService()  {
+        UserBuilder users = User.withDefaultPasswordEncoder();
 
         UserDetails user1 = users
                 .username("zaur")
@@ -39,4 +57,6 @@ public UserDetailsService userDetailsService()  {
 
         return new InMemoryUserDetailsManager(user1, user2, user3);
     }
+
+
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <%--
   Created by IntelliJ IDEA.
   User: iakon
@@ -14,13 +15,19 @@
 <body>
 <h3>Information for all employees</h3>
 <br><br>
-<input type="button" value="Salary"
-    onclick="window.location.href = 'hr_info'">
-Only for HR stuff
+
+<security:authorize access="hasRole('HR')">
+    <input type="button" value="Salary"
+           onclick="window.location.href = 'hr_info'">
+    Only for HR stuff
+</security:authorize>
 <br><br>
-<input type="button" value="Performance"
-       onclick="window.location.href = 'manager_info'">
-Only for Managers
+
+<security:authorize access="hasRole('MANAGER')">
+    <input type="button" value="Performance"
+           onclick="window.location.href = 'manager_info'">
+    Only for Managers
+</security:authorize>
 
 </body>
 </html>
